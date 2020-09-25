d0369009b5efa2285f99e630723efd187527e23e
Tim Ittermann <tim.ittermann@rku-it.de>
2020-09-25 15:07:57 +0200

    Swagger hinzugefügt

--- Backend/Pandemitrac.Server/Startup.cs
+++ Backend/Pandemitrac.Server/Startup.cs
@@ -16,11 +16,14 @@ namespace Pandemitrac.Server
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwaggerConfig();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
