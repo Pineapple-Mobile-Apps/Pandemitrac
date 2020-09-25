b76a716cf30eae63af326060cd36de1fd58ac76d
Böhm Jan <jan.boehm@rku-it.de>
2020-09-25 14:59:22 +0200

    DbContext registrieren

--- Backend/Pandemitrac.Server/Startup.cs
+++ Backend/Pandemitrac.Server/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,6 +25,9 @@ namespace Pandemitrac.Server
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DatabaseContext>(o => {
+                o.UseMySql(Configuration.GetConnectionString("dbConnectionString"));
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
