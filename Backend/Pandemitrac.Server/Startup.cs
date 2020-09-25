eef5eef3fe618af44503e58e88132899e50e6444
Jan <Jan Bhm>
2020-09-25 22:46:31 +0200

    Code aufgeräumt

--- Backend/Pandemitrac.Server/Startup.cs
+++ Backend/Pandemitrac.Server/Startup.cs
@@ -1,15 +1,12 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
-using System.Threading.Tasks;
 using MailKit;
 using MailKit.Net.Smtp;
 using Microsoft.AspNet.OData.Builder;
 using Microsoft.AspNet.OData.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -40,6 +37,9 @@ namespace Pandemitrac.Server
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<ISmtpClient, SmtpClient>();
+            services.AddScoped<MailService>();
+            services.AddScoped<DependentSubjectManager>();
             services.AddScoped<VisitorManager>();
             services.AddSwaggerGen();
             services.AddControllers();
@@ -60,11 +60,6 @@ namespace Pandemitrac.Server
             {
                 o.UseMySql(Configuration.GetConnectionString("dbConnectionString"));
             });
-
-            services.AddTransient<ISmtpClient, SmtpClient>();
-            services.AddScoped<MailService>();
-            services.AddScoped<DependentSubjectManager>();
-            services.AddScoped<VisitorManager>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
