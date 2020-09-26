5147b06549db0ec6241fe58017d523d2fe982b24
Tim Ittermann <tim.ittermann@gmail.com>
2020-09-26 13:34:45 +0200

    VisitEditors Form angepasst

--- pandemitrack.ui/src/VisitsEditor.js
+++ pandemitrack.ui/src/VisitsEditor.js
@@ -1,4 +1,4 @@
-import { Button, CardBody, Card, Label, CardTitle, Input, CardFooter, CardHeader } from "reactstrap";
+import { Button, Form, Row, Col, FormGroup, CardBody, Card, Label, CardTitle, Input, CardFooter, CardHeader } from "reactstrap";
 import React from "react";
 import { createArrayChanger, createInput, createChanger } from "./utils/StateValueTools";
 
@@ -29,24 +29,34 @@ function VisitEditor(props) {
     const changer = createChanger(props.visit, props.setVisit);
 
     return <Card className="mt-3">
+        <Form>
         <CardHeader>
             <CardTitle>Besuchter Ort</CardTitle>
         </CardHeader>
         <CardBody>
+
+        <Row form>
+        <Col md={6}>
+          <FormGroup>
             <Label>Besuchsbeginn</Label>
             <Input type="datetime-local" {...input("Begin")} />
-
+          </FormGroup>
+        </Col>
+        <Col md={6}>
+          <FormGroup>
             <Label>Besuchsende</Label>
             <Input type="datetime-local" {...input("End")} />
-
+          </FormGroup>
+        </Col>
+        </Row>
             <Label>Kontaktdatum</Label>
             <Input type="datetime-local" {...input("Contacted")} />
-
             <LocationEditor location={props.visit.Location} setLocation={e => changer("Location", e)} />
         </CardBody>
         <CardFooter>
             <Button onClick={() => props.setVisit(null)}>Entfernen</Button>
         </CardFooter>
+        </Form>
     </Card>;
 }
 
@@ -57,23 +67,28 @@ function LocationEditor(props) {
     const input = createInput(props.location, props.setLocation);
 
     return <>
-        <Label>Name</Label>
-        <Input {...input("Name")} />
-
-        <Label>Addresse</Label>
-        <Input {...input("Address")} />
-
-        <Label>Postleitzahl</Label>
-        <Input {...input("PostCode")} type="number" />
-
-        <Label>Stadt</Label>
-        <Input {...input("City")} />
-
-        <Label>Kontaktperson</Label>
-        <Input {...input("ContactPerson")} />
-
-        <Label>Kontakttelefonnummer</Label>
-        <Input {...input("Phone")} type="tel" />
+        <Row form>
+        <Col md={6}>
+          <FormGroup>
+            <Label>Name</Label>
+            <Input {...input("Name")} placeholder="Cafe XY" />
+            <Label>Postleitzahl</Label>
+            <Input {...input("PostCode")} type="number" placeholder="44328"/>
+            <Label>Kontaktperson</Label>
+            <Input {...input("ContactPerson")} placeholder="Max Mustermann" />
+          </FormGroup>
+        </Col>
+        <Col md={6}>
+          <FormGroup>
+            <Label>Adresse</Label>
+            <Input {...input("Address")} placeholder="Gleiwitzstraße.200" />
+            <Label>Stadt</Label>
+            <Input {...input("City")} placeholder="Dortmund"/>
+            <Label>Kontakttelefonnummer</Label>
+            <Input {...input("Phone")} type="tel" placeholder="015789346273"/>
+          </FormGroup>
+        </Col>
+      </Row>
     </>
 
 }
