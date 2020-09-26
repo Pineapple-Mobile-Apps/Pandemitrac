4705d7174b67faa1f0b9f9b356056f92d18d9e7c
Jan <Jan Bhm>
2020-09-26 12:07:51 +0200

    Mock-Locations hinzugefügt

--- Backend/Pandemitrac.Server/Logic/Mocker.cs
+++ Backend/Pandemitrac.Server/Logic/Mocker.cs
@@ -54,6 +54,55 @@ namespace Pandemitrac.Server.Logic
             _db.Editors.Add(editor);
             _db.SaveChanges();
 
+            _db.Locations.AddRange(
+                new Location
+                {
+                    Address = "768 Hanson Plaza",
+                    City = "Solna",
+                    ContactPerson = "Tatum Hedges",
+                    Name = "Business-focused asymmetric concept",
+                    Phone = "253-693-6310",
+                    PostCode = 16935
+                },
+                new Location
+                {
+                    Address = "3 Tennessee Street",
+                    City = "Mendi",
+                    ContactPerson = "Delila Rushby",
+                    Name = "Quality-focused systematic matrix",
+                    Phone = "375-925-6848",
+                    PostCode = 16935
+                },
+                new Location
+                {
+                    Address = "01716 Hooker Circle",
+                    City = "Papakura",
+                    ContactPerson = "Pate Conibear",
+                    Name = "Extended directional throughput",
+                    Phone = "977-962-6975",
+                    PostCode = 2113
+                },
+                new Location
+                {
+                    Address = "39 Walton Terrace",
+                    City = "Chornukhyne",
+                    ContactPerson = "Yorker Brockway",
+                    Name = "Profound national local area network",
+                    Phone = "801-867-0793",
+                    PostCode = 2113
+                },
+                new Location
+                {
+                    Address = "93 Vera Hill",
+                    City = "Tayang",
+                    ContactPerson = "Spencer McCurdy",
+                    Name = "Synchronised national info-mediaries",
+                    Phone = "552-444-7262",
+                    PostCode = 2113
+                }
+            );
+            _db.SaveChanges();
+
             var @case = new Case
             {
                 SubjectId = visitors[0].Id,
